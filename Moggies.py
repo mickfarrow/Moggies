aabfaaca8b1103da322b78287f84eea33ee9f6c0
Mick Farrow <mick.farrow@visualdev.net>
2019-10-27 17:39:28 -0500

    Add files via upload

---
+++ Moggies.py
@@ -0,0 +1,163 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+Enter cat age in months: 23
+Human age in years: 26
+Enter cat age in months: 34
+Enter cat age in months: 11
+Human age in years: 14
+Enter cat age in months: x
+Traceback (most recent call last):
+  File "E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py", line 19, in <module>
+    catAge = int(input("Enter cat age in months: "))
+ValueError: invalid literal for int() with base 10: 'x'
+>>> 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+Enter cat age in months: 12
+Human age in years: 15
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): b
+Enter cat age in years: 12
+Human age in years: 64
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+Enter cat age in months: 3
+Traceback (most recent call last):
+  File "E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py", line 23, in <module>
+    writeResult(humanAge)
+TypeError: writeResult() missing 1 required positional argument: 'humanAge'
+>>> 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+Enter cat age in months: 3
+Human age in years: 3
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+Enter cat age in months: 3
+Human age in years: 3
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+
+Enter cat age in months: 3
+Human age in years: 3
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): b
+
+Enter cat age in years: 3
+
+Your cat is at life stage: af
+Human age in years: 28
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+
+Enter cat age in months: 3
+
+Your cat is at life stage: Kitten
+Human age in years: 3
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): x
+
+>>> 
+============ RESTART: E:\Dev\ZakFarrow\COMP101\Farrow_Zak-CA03.py ============
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): b
+
+Enter cat age in years: 18
+
+Your cat is at life stage: Geriatric
+Human age in years: 88
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+
+Enter cat age in months: 5
+
+Your cat is at life stage: Kitten
+Human age in years: 9
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): a
+
+Enter cat age in months: 9
+
+Your cat is at life stage: Junior
+Human age in years: 12
+
+Main Menu-
+Option A: 1 to 24 months
+Option B: 3 to 18 years
+Option X: exit menu
+Select menu option (Enter A, B or X): 
